6338f81e166f410af40bd73a208ee012081db4ea
wuditc1987@163.com <wuditc1987@163.com>
2018-03-08 18:36:31 +0800

    improvement

--- src/main/java/algorithm/Search.java
+++ src/main/java/algorithm/Search.java
@@ -13,13 +13,31 @@ public class Search {
      * @description 二分查找
      * @author wudi
      * @date 2018年3月7日
-     * @param nums 待查找的数组
-     * @param num 需要查找的数字
-     * @return 返回检索次数
+     * @param array 待查找的数组
+     * @param value 需要查找的数字
+     * @return 返回检索次数/返回数据在数组中的位置
      */
-    public static int binarySearch(int[] nums,int num){
-        int count = 0;
-        
+    public static int binarySearch(int[] array, int value){
+        int low = 0;
+        int high = array.length - 1;
+        int count = 1;
+        while(low <= high)
+        {
+            int middle = (low + high) / 2;
+            if(value == array[middle]) //找到则直接返回
+            {
+                return count;
+            }
+            if(value > array[middle]) //若数据大于数组中的数，则从middle后一位开始计算，重新查找
+            {
+                low = middle + 1;
+            }
+            if(value < array[middle])
+            {
+                high = middle - 1;
+            }
+            count ++;
+        }
         return count;
     }
     
@@ -37,7 +55,8 @@ public class Search {
     
 
     public static void main(String[] args) {
-
+        int[] nums = new int[]{5,8,14,20,31,55,78,81,93,97,111};
+        System.out.println(binarySearch(nums,93));
     }
 
 }
--- src/main/java/algorithm/Sort.java
+++ src/main/java/algorithm/Sort.java
@@ -10,8 +10,16 @@ package algorithm;
 public class Sort {
     
     /**
+     * 冒泡排序
+     * @description 它重复地走访过要排序的数列，一次比较两个元素，如果它们的顺序错误就把它们交换过来。
+     *      走访数列的工作是重复地进行直到没有再需要交换，也就是说该数列已经排序完成。这个算法的名字由来是因为越小的元素会经由交换慢慢“浮”到数列的顶端。 
+     * 算法描述 
+     *      1.比较相邻的元素。如果第一个比第二个大，就交换它们两个；
+     *      2.对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对，这样在最后的元素应该会是最大的数；
+     *      3.针对所有的元素重复以上的步骤，除了最后一个；
+     *      4.重复步骤1~3，直到排序完成。
      * 
-     * @description 冒泡排序
+     * 算法分析 最佳情况：T(n) = O(n)   最差情况：T(n) = O(n2)   平均情况：T(n) = O(n2)
      * @author wudi
      * @date 2018年3月7日
      * @param nums
@@ -27,7 +35,6 @@ public class Sort {
                 }
             }
         }
-        System.out.println(nums);
         return nums;
     }
     
@@ -59,7 +66,6 @@ public class Sort {
             }
             k--;//减小一次排序的尾边界
         }//end while
-        
         return arr;
     }
     
@@ -91,9 +97,133 @@ public class Sort {
                     flag = j;//记录最新的尾边界.
                 }
             }
+        }//end while
+        return arr;
+    }
+    
+    /**
+     * 选择排序
+     * @description 表现最稳定的排序算法之一，因为无论什么数据进去都是O(n2)的时间复杂度，所以用到它的时候，数据规模越小越好。
+     *      唯一的好处可能就是不占用额外的内存空间了吧。理论上讲，选择排序可能也是平时排序一般人想到的最多的排序方法了吧。
+     *      选择排序(Selection-sort)是一种简单直观的排序算法。
+     *      它的工作原理：首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+     *      以此类推，直到所有元素均排序完毕。 
+     * 算法描述 :  n个记录的直接选择排序可经过n-1趟直接选择排序得到有序结果。
+     *      1.初始状态：无序区为R[1..n]，有序区为空；
+     *      2.第i趟排序(i=1,2,3…n-1)开始时，当前有序区和无序区分别为R[1..i-1]和R(i..n）。
+     *   该趟排序从当前无序区中-选出关键字最小的记录 R[k]，将它与无序区的第1个记录R交换，使R[1..i]和R[i+1..n)分别变为记录个数增加1个的新有序区和记录个数减少1个的新无序区；
+     *      3.n-1趟结束，数组有序化了。
+     * @author wudi
+     * @date 2018年3月8日
+     * @param nums
+     * @return
+     */
+    public int[] selection(int[] nums){
+        int len = nums.length;
+        int minIndex, temp;
+        for (int i = 0; i < len - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < len; j++) {
+                if (nums[j] < nums[minIndex]) {     //寻找最小的数
+                    minIndex = j;                 //将最小数的索引保存
+                }
+            }
+            temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        return nums;
+    }
+    
+    /**
+     * 插入排序
+     * @description 它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+     *      插入排序在实现上，通常采用in-place排序（即只需用到O(1)的额外空间的排序），因而在从后向前扫描过程中，需要反复把已排序元素逐步向后挪位，为最新元素提供插入空间。
+     * 算法描述
+     *      1.从第一个元素开始，该元素可以认为已经被排序；
+     *      2.取出下一个元素，在已经排序的元素序列中从后向前扫描；
+     *      3.如果该元素（已排序）大于新元素，将该元素移到下一位置；
+     *      4.重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+     *      5.将新元素插入到该位置后；
+     *      6.重复步骤2~5。
+     * 
+     * 算法分析  最佳情况：T(n) = O(n) 最坏情况：T(n) = O(n2) 平均情况：T(n) = O(n2)
+     * 
+     * @author wudi
+     * @date 2018年3月7日
+     * @param nums
+     * @return
+     */
+    public int[] insertion (int[] nums){
+        int len = nums.length;
+        int preIndex,current;
+        for(int i = 1; i < len; i++){
+            preIndex = i-1;
+            current = nums[i];
+            while (preIndex >= 0 && nums[preIndex] > current) {
+                nums[preIndex] = nums[preIndex];
+                preIndex --;
+            }
+            nums[preIndex - 1] = current;
+        }
+        return nums;
+    }
+    
+    /**
+     * 希尔排序
+     * @description 1959年Shell发明，第一个突破O(n^2)的排序算法，是简单插入排序的改进版。它与插入排序的不同之处在于，它会优先比较距离较远的元素。希尔排序又叫缩小增量排序
+     * 算法描述 :先将整个待排序的记录序列分割成为若干子序列分别进行直接插入排序
+     *      1.选择一个增量序列t1，t2，…，tk，其中ti>tj，tk=1；
+     *      2.按增量序列个数k，对序列进行k 趟排序；
+     *      3.每趟排序，根据对应的增量ti，将待排序列分割成若干长度为m 的子序列，分别对各子表进行直接插入排序。
+     *      仅增量因子为1时，整个序列作为一个表来处理，表长度即为整个序列的长度。
+     * 
+     * 算法分析 :最佳情况：T(n) = O(nlog2 n)  最坏情况：T(n) = O(nlog2 n)  平均情况：T(n) =O(nlog n)　
+     * 
+     * @author wudi
+     * @date 2018年3月8日
+     * @param arr
+     * @return
+     */
+    public int[] shell(int[] arr){
+        int len = arr.length,temp,gap = 1;
+        while(gap < len/3) {          //动态定义间隔序列(步长)
+            gap =gap*3+1;
+        }
+        for (; gap> 0; gap = (int) Math.floor(gap/3)) {
+            for (int i = gap; i < len; i++) {
+                temp = arr[i];
+                for (int j = i-gap; j > 0 && arr[j]> temp; j-=gap) {
+                    arr[j+gap] = arr[j];
+                }
+                arr[i+gap] = temp;
+            }
         }
         return arr;
     }
+    
+    /**
+     * 归并排序
+     * @description 和选择排序一样，归并排序的性能不受输入数据的影响，但表现比选择排序好的多，因为始终都是O(n log n）的时间复杂度。代价是需要额外的内存空间。
+     *      归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。
+     *      归并排序是一种稳定的排序方法。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并
+     * 算法描述 
+     *      1.把长度为n的输入序列分成两个长度为n/2的子序列；
+     *      2.对这两个子序列分别采用归并排序；
+     *      3.将两个排序好的子序列合并成一个最终的排序序列。
+     *      
+     * 算法分析  最佳情况：T(n) = O(n)  最差情况：T(n) = O(nlogn)  平均情况：T(n) = O(nlogn)
+     * 
+     * @author wudi
+     * @date 2018年3月8日
+     * @param arr
+     * @return
+     */
+    public int[] merge(int[] arr){
+        
+        return arr;
+    }
+    
     
     /**
      * 
@@ -108,28 +238,6 @@ public class Sort {
         return nums;
     }
     
-    /**
-     * 
-     * @description 插入排序
-     * @author wudi
-     * @date 2018年3月7日
-     * @param nums
-     * @return
-     */
-    public int[] insert(int[] nums){
-        int len = nums.length;
-        int preIndex,current;
-        for(int i = 1; i < len; i++){
-            preIndex = i-1;
-            current = nums[i];
-            while (preIndex >= 0 && nums[preIndex] > current) {
-                nums[preIndex] = nums[preIndex];
-                preIndex --;
-            }
-            nums[preIndex - 1] = current;
-        }
-        return nums;
-    }
     
     /**
      * 
