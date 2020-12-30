a70217a9e91d95f7946c8a8a4bb12465827eac21
wuditc1987 <wuditc1987@163.com>
2020-12-30 10:07:02 +0800

    Update Sort.java

--- src/main/java/algorithm/Sort.java
+++ src/main/java/algorithm/Sort.java
@@ -25,8 +25,8 @@ public class Sort {
      * @return
      */
     public static int[] bubble(int[] nums){
-        for(int i = 1;i<nums.length;i++){
-            for(int j = 0; j< nums.length - i; j++){
+        for(int i = 0;i<nums.length-1;i++){
+            for(int j = 0; j< nums.length - i - 1; j++){
                 if(nums[j] > nums[j+1]){
                     int temp = nums[j];
                     nums[j] = nums[j+1];
